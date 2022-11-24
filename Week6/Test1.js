0de04cc3ded0fb9af940da0dc115ccce21ee2851
Thitikorn <64309010005@tnk.ac.th>
2022-11-24 15:27:52 +0700

    Create Test1.js

---
+++ Week6/Test1.js
@@ -0,0 +1,80 @@
+let obj = {
+    "10001" : {
+        "First-lastname": "นางสาวชนิสา ไกรรอด",
+        "Department" : "ไอที",
+        "position" : "System Engineer",
+        "salary" : 42000 ,
+        "Bonus Ranking" : "S"
+},
+    "10002" : {
+        "First-lastname": "นายวศพล สิริอนันต",
+        "Department" : "ไอที",
+        "position" : "System Engineer",
+        "salary" : 13000,
+        "Bonus Ranking" : "A"
+},
+    "10003" : {
+        "First-lastname": "นางจิราพร อินทรประเสริฐ",
+        "Department" : "ฝ่ายบุคคล",
+        "position" : "HR",
+        "salary" : 9000,
+        "Bonus Ranking" : "C"
+},
+    "10004" : {
+        "First-lastname": "นางสาวพิมพา กาญจนมาศ",
+        "Department" : "ฝ่ายบุคคล",
+        "position" : "HR",
+        "salary" : 21000 ,
+        "Bonus Ranking" : "B"
+},
+    "10005" : {
+        "First-lastname": "นายจิรโชติ วัฒนโกศล",
+        "Department" : "ฝ่ายบุคคล",
+        "position" : "HR",
+        "salary" : 22000,
+        "Bonus Ranking" : "A"
+},
+    "10006" : {
+        "First-lastname": "นายธัชพล ขจรเกียรติสกุล",
+        "Department" : "การเงิน",
+        "position" : "Account",
+        "salary" : 12000 ,
+        "Bonus Ranking" : "S"
+},
+    "10007" : {
+        "First-lastname": "นายธนวิทย สุวรรณโชติ",
+        "Department" : "การเงิน",
+        "position" : "Account",
+        "salary" : 14000,
+        "Bonus Ranking" : "A"
+},
+    "10008" : {
+        "First-lastname": "นางพัชรียา สําราญทรัพย์",
+        "Department" : "การเงิน",
+        "position" : "Account",
+        "salary" : 50000 ,
+        "Bonus Ranking" : "C"
+},
+    "10009" : {
+        "First-lastname": "นางสาวขวัญชนก ธาดาวรวงศ์",
+        "Department" : "ไอที",
+        "position" : "System Engineer",
+        "salary" : [16000] ,
+        "Bonus Ranking" : "A"
+}
+};
+
+let BonusPercen = {
+    "level_S" : {
+        "BonusRate": 1.4,
+},
+    "level_A" : {
+        "BonusRate": 1.2,
+},
+    "level_B" : {
+        "BonusRate": 1.0,
+},
+    "level_C" : {
+        "BonusRate": 0.8,
+},
+}
