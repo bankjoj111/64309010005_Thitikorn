b12addf9b12029a5d5dd4a7abf8e53025e7be705
Thitikorn <64309010005@tnk.ac.th>
2022-11-24 10:44:03 +0700

    Create reviewJS.js

---
+++ Week5/reviewJS.js
@@ -0,0 +1,10 @@
+x = 10;
+var y = 15;
+let myName = 'Thitikorn';
+const yar = 2022;
+//year = 2023;
+console.log('Year เป็นชนิด ',typeof(y));
+console.log('Myname เป็นชนิด ',typeof(myName));
+
+y='tnk';
+console.log(y, typeof(y));
