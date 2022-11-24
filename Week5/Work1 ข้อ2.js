1538746f9030a1b18d5345212a166c94c282575f
Thitikorn <64309010005@tnk.ac.th>
2022-11-24 10:44:12 +0700

    Create Work1 ข้อ2.js

---
+++ Week5/Work1 ข้อ2.js
@@ -0,0 +1,13 @@
+//ข้อ2
+const data3 = {
+    "Customer" : "Mr.Thiti", 
+    "item Order " : ["Ram","Monitor","Mouse"],
+    "QTY" : [3,1,2],
+    "Price" : [1200,2500,500]
+}
+
+//ข้อ3
+for(i=0; i< data.length; i++){
+
+    console.log(data[i]);
+}
