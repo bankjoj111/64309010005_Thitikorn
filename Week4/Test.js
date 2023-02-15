3a53a0c12bb098706d42b8f9a1902fef67522176
Thitikorn <64309010005@tnk.ac.th>
2023-02-15 11:05:56 +0700

    Thiti

---
+++ Week4/Test.js
@@ -0,0 +1,12 @@
+console.log(5);
+let myName = 'Thitikorn';
+let lastName = 'Wichaidit';
+
+const year = 2022;
+
+
+let grade = 3.56;
+grade = Math.floor(grade);
+console.log(grade);
+
+console.log(`MY name is ${myName}`)
